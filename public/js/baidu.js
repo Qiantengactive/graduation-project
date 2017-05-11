a4047ce92b63dc45a439837552199bde5bd806e9
936129056@qq.com <936129056@qq.com>
2017-05-11 17:51:19 +0800

    5月11日第二次提交

---
+++ public/js/baidu.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?808d451d8229683cdc453b8ecef2d09b";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+</script>
