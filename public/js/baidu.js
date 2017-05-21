79a0859e4302b75c9a2b0925dc55e63d9924a307
936129056@qq.com <936129056@qq.com>
2017-05-21 11:23:00 +0800

    修改

--- public/js/baidu.js
+++ public/js/baidu.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?808d451d8229683cdc453b8ecef2d09b";
+    hm.src = "https://hm.baidu.com/hm.js?22b6731583d1ab322457e390eb977c6d";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
